d2c805a658a806b78fbfa760d5a32454de652a3b
ahme2001 <ahmedaboeleid30@gmail.com>
2024-03-19 03:33:14 +0200

    Handle request in profile page

--- lib/profile_view.dart
+++ lib/profile_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:rosheta_ui/generated/l10n.dart';
 import 'package:rosheta_ui/models/profile_model.dart';
 import 'package:rosheta_ui/services/Profile_service.dart';
@@ -15,15 +14,22 @@ class ProfileView extends StatefulWidget {
 class _ProfileViewState extends State<ProfileView> {
   late Future<Profile> _profileFuture;
 
-  @override
-  Future<void> initState() async {
-    super.initState();
-    ProfileApi pa = new ProfileApi();
-    _profileFuture = await pa.fetchProfile();
+  // create constructor to initialize the future
+
+  Future<Profile> _fetchProfile() async {
+    try {
+      ProfileApi pa = ProfileApi();
+      return await pa.featchProfile();
+    } catch (e) {
+      // Handle any errors that occur during the data fetching process
+      print('Error fetching profile: $e');
+      throw Exception('Failed to fetch profile');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    _profileFuture = _fetchProfile();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.cyan,
@@ -93,66 +99,6 @@ class _ProfileViewState extends State<ProfileView> {
     );
   }
 }
-
-// class ProfileView extends StatelessWidget {
-//   const ProfileView({super.key});
-//   final Color iconColor = const Color.fromARGB(255, 9, 19, 19);
-
-//   @override
-//   Widget build(BuildContext context) {
-// return Scaffold(
-//   appBar: AppBar(
-//     backgroundColor: Colors.cyan,
-//     title: Text(
-//       S.of(context).title,
-//       style: const TextStyle(
-//         fontWeight: FontWeight.bold,
-//         fontSize: 25,
-//         color: Colors.white,
-//       ),
-//     ),
-//     centerTitle: true,
-//   ),
-//       body: Container(
-//         color: const Color.fromARGB(
-//             255, 233, 255, 255), // Set your desired background color here
-//         child: Padding(
-//           padding: EdgeInsets.all(15.0),
-//           child: Center(
-//             child: SingleChildScrollView(
-//               child: Column(
-// children: [
-//   const CircleAvatar(
-//     radius: 100.0,
-//     backgroundImage: AssetImage('Images/profile.png'),
-//   ),
-//   const Text(
-//     "Mohamed Mostafa Ibrahim",
-//     style: TextStyle(
-//         fontSize: 25.5, color: Color.fromARGB(255, 1, 14, 15)),
-//   ),
-//   const SizedBox(height: 30),
-//   getCont(Icons.email, "Mohamedmostaf@gmail.com",
-//       S.of(context).email),
-//   const SizedBox(height: 10),
-//   getCont(Icons.phone, "01032901480", S.of(context).Phone),
-//   const SizedBox(height: 10),
-//   getCont(
-//     Icons.date_range,
-//     "3/18/2024",
-//     S.of(context).UserbirthDate,
-//   ),
-//   const SizedBox(height: 10),
-//   getCont(Icons.credit_card, "30107120202336",
-//       S.of(context).NationalId),
-// ],
-//               ),
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
 
 Column getCont(IconData icon, dynamic st, String text) {
   return Column(
--- lib/services/Profile_service.dart
+++ lib/services/Profile_service.dart
@@ -4,13 +4,12 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ProfileApi {
-  Future<Profile> featchProfile(String email, String password) async {
+  Future<Profile> featchProfile() async {
     const url = 'http://192.168.1.2:5000/profile';
     try {
-      String accessToken =
-          await getAccessToken(); // Assuming this method gets the access token
-
-// Perform the GET request
+      String accessToken = await getAccessToken(); // Assuming this method gets the access token
+          // "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRvdG9AZ21haWwuY29tIiwiaWQiOiI2NWY4YzBlM2YwMzcwMzQxZjJlYjEwZDAiLCJpYXQiOjE3MTA4MTEzNDAsImV4cCI6MTcxMDgxNDk0MH0.QF5dOVusdSxLbL8ihRb3y973w_TA-7xbGlXIhvjBldU";
+      
       http.Response response = await http.get(
         Uri.parse(url),
         headers: {
@@ -19,14 +18,12 @@ class ProfileApi {
               'Bearer $accessToken', // Include access token in Authorization header
         },
       );
-
+      
       // Deserialize body to be accessible
       if (response.statusCode == 200 || response.statusCode == 201) {
         String data = response.body;
         var jsonData = jsonDecode(data);
         Profile dataProfile = Profile.fromJson(jsonData);
-        // store tokens locally to be accessible again
-        // await prefs.setString('refreshtoken', tokens.refreshToken);
         return dataProfile;
       } else {
         print('Status code: ${response.statusCode}');
@@ -41,6 +38,4 @@ class ProfileApi {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getString('acesstoken') ?? '';
   }
-
-  fetchProfile() {}
 }
