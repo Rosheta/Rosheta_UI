22f7252af8691edda51d709d1a85f66e234531af
Mohamed_euler <96792977+mohamed-euler@users.noreply.github.com>
2024-03-20 05:12:37 +0200

    Add edit profile feature

---
+++ lib/services/edit_profile_service.dart
@@ -0,0 +1,35 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class EditProfileApi {
+  Future<bool> editprofile(
+      {required email,
+      required name,
+      required phone,
+      required ssn,
+      required birthdate}) async {
+    String url = 'http://192.168.1.2:5000/editProfile';
+    try {
+      http.Response response = await http.post(
+        Uri.parse(url),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode({
+          'email': email,
+          'name': name,
+          'phone': phone,
+          'ssn': ssn,
+          'birthdate': birthdate,
+        }),
+      );
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        return true;
+      } else
+        return false;
+    } catch (e) {
+      print("Exception: $e");
+      return false;
+    }
+  }
+}
