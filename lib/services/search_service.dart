6debbd8bf399e17d4a683e1986712dff0348e6fd
mohamedaiad <mohamedaiad2000@gmail.com>
2024-03-25 08:05:55 +0200

    name api changed and failed to make post req

--- lib/services/search_service.dart
+++ lib/services/search_service.dart
@@ -10,11 +10,12 @@ class search_service {
     // final Uri uri = Uri.parse('http://192.168.1.7:3000/api/data?query=$query'); // for actual mobile over wifi network
 
     final String apiUrl = dotenv.env['API_URL']!;
-    final uri = '$apiUrl/api/data?query=$query';
+    final uri = '$apiUrl/search?query=$query';
     print('Fetching suggestions for query: $query'); // Check if the query is correct
     try {
       print("enter try");
       final response = await http.get(Uri.parse(uri));
+      // final response = await http.post(Uri.parse(uri), body: json.encode({'query': query}));
       if (response.statusCode == 200) {
         final List<dynamic> data = json.decode(response.body);
         return data;
